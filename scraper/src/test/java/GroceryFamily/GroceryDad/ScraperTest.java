0fc7cc582fec07d74a4845b163752d04166fcacb
Vladimir Pavlovskii <antivoland@gmail.com>
2023-07-28 12:08:34 +0300

    disable scraper test

--- scraper/src/test/java/GroceryFamily/GroceryDad/ScraperTest.java
+++ scraper/src/test/java/GroceryFamily/GroceryDad/ScraperTest.java
@@ -1,8 +1,10 @@
 package GroceryFamily.GroceryDad;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("Multiple Spring Boot applications, some of them utilize ChromeDriver")
 @SpringBootTest(classes = Scraper.class)
 class ScraperTest {
     @Test
