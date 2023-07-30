886408c96768e778807cf773e99643501b24cad4
Vladimir Pavlovskii <antivoland@gmail.com>
2023-07-30 10:01:52 +0300

    move secrets to external file

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # BackEnd
 
 [![Build Status](https://github.com/GroceryFamily/BackEnd/workflows/build/badge.svg)](https://github.com/GroceryFamily/BackEnd/actions/workflows/build.yml)
+
+## Secrets
+
+Create `secrets/application-secrets.yaml` in the project's root directory:
+
+```yaml
+spring.datasource:
+  username: MYSQL_USERNAME
+  password: MYSQL_PASSWORD
+```
+
+Replace `MYSQL_USERNAME` and `MYSQL_PASSWORD` with actual values.
--- core/src/main/java/GroceryFamily/GroceryElders/GroceryEldersApplicationConfig.java
+++ core/src/main/java/GroceryFamily/GroceryElders/GroceryEldersApplicationConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
@@ -11,4 +12,5 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(basePackages = {"GroceryFamily"})
 @EntityScan(basePackages = {"GroceryFamily"})
 @ConfigurationPropertiesScan(basePackages = "GroceryFamily")
+@PropertySource(value="file:${user.dir}/secrets/application-secrets.yaml")
 public class GroceryEldersApplicationConfig {}
