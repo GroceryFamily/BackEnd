f878a397923817bda711017293790e1c17484da3
Vladimir Pavlovskii <antivoland@gmail.com>
2023-08-13 14:19:38 +0300

    cleanup

--- scraper/src/main/java/GroceryFamily/GroceryDad/scraper/PrismaScraper.java
+++ scraper/src/main/java/GroceryFamily/GroceryDad/scraper/PrismaScraper.java
@@ -2,51 +2,17 @@ package GroceryFamily.GroceryDad.scraper;
 
 import GroceryFamily.GroceryDad.scraper.page.Node;
 import GroceryFamily.GroceryDad.scraper.page.context.PrismaContext;
-import GroceryFamily.GroceryDad.scraper.tree.CategoryTreePath;
-import GroceryFamily.GroceryDad.scraper.view.NewCategoryView;
-import GroceryFamily.GroceryDad.scraper.view.Path;
 import GroceryFamily.GroceryElders.domain.Product;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
 import java.util.function.Consumer;
-
-import static GroceryFamily.GroceryDad.scraper.PrismaPage.productElements;
-import static GroceryFamily.GroceryDad.scraper.PrismaPage.topCategoryElements;
-import static GroceryFamily.GroceryDad.scraper.page.Page.sleep;
-import static com.codeborne.selenide.CollectionCondition.itemWithText;
-import static com.codeborne.selenide.Condition.visible;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 @Slf4j
 @SuperBuilder
 class PrismaScraper extends Scraper {
     @Override
-    protected void acceptOrRejectCookies() {
-        $("*[class*='js-cookie-notice'] *[class='close-icon']").shouldBe(visible).click();
-        sleep();
-    }
-
-    @Override
-    protected void switchToEnglish() {
-        $("*[data-language='en']").shouldBe(visible).click();
-        topCategoryElements().shouldHave(itemWithText("Groceries"));
-        sleep();
-    }
-
-    @Override
     protected void scrap(Consumer<Product> handler) {
         Node.root(rootURL(), new PrismaContext(cacheFactory(), categoryPermissions)).traverse(handler);
-    }
-
-    private static List<Product> products(CategoryTreePath path) {
-        return productElements()
-                .asFixedIterable()
-                .stream()
-                .map(PrismaProductView::new)
-                .map(view -> view.product(path))
-                .toList();
     }
 }
