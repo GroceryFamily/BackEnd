f5ca1df8a393b1fc5f4d55a68ac717e64c426a73
Vladimir Pavlovskii <antivoland@gmail.com>
2023-08-13 13:30:40 +0300

    cleanup

--- scraper/src/main/java/GroceryFamily/GroceryDad/scraper/tree/CategoryLinkTree.java
+++
@@ -1,9 +0,0 @@
-package GroceryFamily.GroceryDad.scraper.tree;
-
-import GroceryFamily.GroceryDad.scraper.page.link.CategoryLink;
-
-public class CategoryLinkTree extends Tree<String, CategoryLink> {
-    public void add(CategoryLink link) {
-        add(link.codePath.segments(), link);
-    }
-}
