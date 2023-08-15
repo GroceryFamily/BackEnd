bb57a0e1ded07568c81bb708ba4416777fd8b957
Vladimir Pavlovskii <antivoland@gmail.com>
2023-08-15 18:52:16 +0300

    cleanup

--- scraper/src/main/java/GroceryFamily/GroceryDad/scraper/page/Link.java
+++ scraper/src/main/java/GroceryFamily/GroceryDad/scraper/page/Link.java
@@ -7,18 +7,10 @@ import lombok.ToString;
 @Builder
 @ToString
 public class Link {
-    @Deprecated
-    public final Path<String> codePath; // todo: source code path, code
-
     public final String code;
     public final String name;
     public final String url;
     public final Source source;
-
-    @Deprecated
-    public Path<String> sourceCodePath() {
-        return source != null ? source.codePath() : Path.empty();
-    }
 
     public Path<String> codePath() {
         return source != null ? source.codePath().followedBy(code) : Path.empty();
@@ -26,11 +18,6 @@ public class Link {
 
     public Path<String> namePath() {
         return source != null ? source.namePath().followedBy(name) : Path.empty();
-    }
-
-    @Deprecated
-    public String code() {
-        return codePath.tail();
     }
 
     public static Link category(Category category, Source source) {
--- scraper/src/main/java/GroceryFamily/GroceryDad/scraper/page/PageUtils.java
+++ scraper/src/main/java/GroceryFamily/GroceryDad/scraper/page/PageUtils.java
@@ -1,16 +1,10 @@
 package GroceryFamily.GroceryDad.scraper.page;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 
-import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.executeJavaScript;
-import static java.lang.String.format;
 
 public class PageUtils {
     public static Duration sleepDelay = Duration.ofSeconds(1);
@@ -25,29 +19,5 @@ public class PageUtils {
 
     public static void scrollDown() {
         executeJavaScript("scroll(0, document.body.scrollHeight);");
-    }
-
-    public static String decodeUrl(String url) {
-        return URLDecoder.decode(url, StandardCharsets.UTF_8);
-    }
-
-    public static Condition hrefContains(String value) {
-        return attributeMatching("href", format(".*%s.*", value));
-    }
-
-    public static Condition textContains(String value) {
-        return matchText(format(".*%s.*", value));
-    }
-
-    public static Condition number() {
-        return matchText("[0-9]+");
-    }
-
-    public static Condition number(Number number) {
-        return text(number.toString());
-    }
-
-    public static String html() {
-        return $("html").innerHtml();
     }
 }
