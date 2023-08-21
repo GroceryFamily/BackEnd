dcb8e59d386756597393fbb2ea79f1b75d86a5b4
DigiDoggy <aituganov.ilja@gmail.com>
2023-08-21 11:10:14 +0300

    cleanUp

--- server/src/test/java/GroceryFamily/GroceryMom/api/ProductAPITest.java
+++ server/src/test/java/GroceryFamily/GroceryMom/api/ProductAPITest.java
@@ -4,25 +4,21 @@ import GroceryFamily.GroceryElders.domain.Page;
 import GroceryFamily.GroceryElders.domain.Price;
 import GroceryFamily.GroceryElders.domain.Product;
 import GroceryFamily.GroceryMom.service.ProductService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 
-import javax.xml.crypto.Data;
 import java.math.BigDecimal;
 import java.util.*;
 
-import static java.util.stream.DoubleStream.builder;
 import static org.hamcrest.Matchers.is;
-import static net.bytebuddy.matcher.ElementMatchers.isGenericGetter;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -41,28 +37,9 @@ class ProductAPITest {
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
+
         this.mockMvc = MockMvcBuilders.standaloneSetup(productAPI).build();
 
-    }
-
-    @Test
-    void testListProductsByPageSize() throws Exception{
-
-        Page<Product> testPage = Page.<Product>builder()
-                .content(products)
-                .nextPageToken("some_Token")
-                .build();
-        
-        when(service.list(anyInt())).thenReturn(testPage);
-
-        mockMvc.perform(MockMvcRequestBuilders.get("/products?pageSize=10"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.nextPageToken", is("some_Token")));
-
-    }
-
-    @Test
-    void testListProductsByPageToken() throws Exception{
         Set<Price> prices= new HashSet<>();
         Price price = Price.builder()
                 .unit("pc")
@@ -82,11 +59,33 @@ class ProductAPITest {
                 .name("Rock 100 kg")
                 .prices(prices)
                 .build();
-        List<Product> products=new ArrayList<>();
+        this.products=new ArrayList<>();
         products.add(product);
         products.add(product1);
 
-        System.out.println(products.size());
+
+    }
+
+
+    @Test
+    void testListProductsByPageSize() throws Exception{
+
+        Page<Product> testPage = Page.<Product>builder()
+                .content(products)
+                .nextPageToken("some_Token")
+                .build();
+        
+        when(service.list(anyInt())).thenReturn(testPage);
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/products?pageSize=10"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.nextPageToken", is("some_Token")));
+
+    }
+
+    @Test
+    void testListProductsByPageToken() throws Exception{
+
         Page<Product> testPage = Page.<Product>builder()
                 .content(products)
                 .nextPageToken("some_Token")
@@ -98,6 +97,17 @@ class ProductAPITest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.nextPageToken", is("some_Token")))
                 .andExpect(jsonPath("$.content.length()", is(2)));
+
+    }
+
+    @Test
+    void getProductByIdTest(){
+
+        when(service.get("zoo.buy:32133")).thenReturn(products.get(0));
+
+        Product actualProduct = productAPI.get("zoo.buy:32133");
+
+        Assertions.assertEquals(products.get(0), actualProduct);
 
     }
 
